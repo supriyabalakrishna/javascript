ffe390af224a883cfcf7421d1377a9ad522dd5bb
supriyabalakrishna <supriyabalakrishna598@gmail.com>
2025-12-02 18:55:16 +0530

    Add files via upload

---
+++ l.js
@@ -0,0 +1,46 @@
+const student = {
+    name : "supriya",
+    age : 44,
+    salary : 9000000000000  
+} 
+const product = {
+ name : "parker jotter standard",
+ price : 270,
+ review : 4,
+ color:"black"
+
+}
+console.log(product["price"]);
+const profile = 
+{
+    name : "supriya",
+    posts : 115,
+    followers : 5690000,
+    following : 4,
+    role : "entreprenuer"
+}
+console.log(profile);
+//comments single line
+/*hello multiple line comments */
+/* operators 
+arithmetic operators
++ - * / % ++ --    
+assignment operators
+= += -= *= /= %=
+comparison operators
+== === != !== > < >= <=
+logical operators
+&& || !
+bitwise operators
+& | ^ ~ << >> >>>
+*/
+let x = 10;
+let y = 20;
+console.log(x+y);
+console.log(x*y);
+console.log(x-y);
+console.log(x/y);
+console.log(x%y);
+console.log(x**y);
+console.log(x++);
+console.log(++x);
