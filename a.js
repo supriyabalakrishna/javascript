80b1a5e0f976d68630339096b42920475bc648c0
supriyabalakrishna <supriyabalakrishna598@gmail.com>
2025-12-02 18:53:09 +0530

    Add files via upload

---
+++ a.js
@@ -0,0 +1,30 @@
+const a = 10;
+const b = 30;
+console.log(a+b);
+//variables are containers to store data
+//data stored in memory locations
+var name = "supriya";
+let age = 22;
+console.log(name);
+console.log(age);
+x = undefined;
+isfollow = true;
+console.log(isfollow);
+// = is assignment operator
+// == equal to operator
+// === strict equal to operator
+//variables should not start with numbers
+//should not contain spaces
+//should not be a keyword
+//should be meaningful 
+//variables are case sensitive
+//var: a global scope and function scope and can be re-assigned
+//let: block scope variable and cannot be re-assigned but can be updated
+//const: block scope and cannot be re-assigned
+const student = {
+    name : "supriya",
+    age : 44,
+    salary : 9000000000000
+}
+console.log(student.salary);
+console.log(student);
