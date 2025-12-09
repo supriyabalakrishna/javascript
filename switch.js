441f02fbd5d965c237e37265cb8a03c00dbecac1
supriyabalakrishna <supriyabalakrishna598@gmail.com>
2025-12-09 19:26:17 +0530

    Add files via upload

---
+++ switch.js
@@ -0,0 +1,28 @@
+const exp = "apple";
+switch(exp)
+{
+    case "apple":
+        console.log("3$ per bag");
+        break;
+        case "mango":
+            console/log("4$ per bag");
+            break;
+            case "kiwi":
+                console.log("10$ per bag");
+                break;
+                default:
+                    console.log("not in stock");
+                    break;
+}
+
+/* syntax
+const expression = value;
+switch(expression)
+{   case "value1":
+        //code to be executed
+        break;      
+        default:
+            //code to be executed
+            break;
+}
+*/
