391442c047e09de9f35e36ae45187579ef88bcbe
supriyabalakrishna <supriyabalakrishna598@gmail.com>
2025-12-09 20:33:34 +0530

    Add files via upload

---
+++ p1.js
@@ -0,0 +1,10 @@
+const prompt = require("prompt-sync")();
+let num = Number(prompt("enter a number:"));
+if(num%5==0)
+{
+    console.log(num + " is divisible by 5");            
+
+}
+else{
+    console.log(num + " is not divisible by 5");
+}
