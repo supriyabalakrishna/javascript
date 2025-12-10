e8684d4550aa569c7266d323b03627d0fac9e6c9
supriyabalakrishna <supriyabalakrishna598@gmail.com>
2025-12-10 19:28:52 +0530

    Add files via upload

---
+++ infinteloop.js
@@ -0,0 +1,11 @@
+/*const prompt = require("prompt-sync")();
+let n = Number(prompt("enter a number"));
+for(i = 0 ; i< n ; i++)
+{
+    console.log("hello world");
+}*/
+//infinite loop
+for(let i = 1 ; i >= 0 ; i++)
+{
+    console.log(i);
+}
