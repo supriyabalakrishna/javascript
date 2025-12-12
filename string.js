5d6677761d015ec8db6cd95f0176eb864eeac74f
supriyabalakrishna <supriyabalakrishna598@gmail.com>
2025-12-12 20:50:15 +0530

    Add files via upload

---
+++ string.js
@@ -0,0 +1,6 @@
+var  str = "supriya balakrishna      ";
+console.log(str.length)
+console.log(str.toUpperCase());
+console.log(str.toLowerCase());
+console.log(str.toLocaleLowerCase());
+console.log(str.trim());
