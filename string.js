9df4299f418abda81c414d1be8d15565a256d253
supriyabalakrishna <supriyabalakrishna598@gmail.com>
2025-12-15 21:11:51 +0530

    Delete string.js

--- string.js
+++
@@ -1,6 +0,0 @@
-var  str = "supriya balakrishna      ";
-console.log(str.length)
-console.log(str.toUpperCase());
-console.log(str.toLowerCase());
-console.log(str.toLocaleLowerCase());
-console.log(str.trim());
